d41bceefeb1fb6ead4f544f30b6b41ba978ca800
Peter Parker <xanderon.git@hotmail.com>
2026-06-04 03:45:58 +0300

    feat(youtube-plugin): add package skeleton, update registry

---
+++ plugins/mywatch-plugin-youtube/src/index.tsx
@@ -0,0 +1,9 @@
+import type { MyWatchPlugin } from '@mywatch/plugin-sdk'
+
+const plugin: MyWatchPlugin = {
+  id: 'youtube',
+  displayName: 'YouTube Links',
+  listTypes: [],
+}
+
+export default plugin
