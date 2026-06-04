e5bcae1a86ea0fc217d2d159a6d96b33391bca14
Peter Parker <xanderon.git@hotmail.com>
2026-06-04 03:50:01 +0300

    feat(youtube-plugin): plugin entry point

--- plugins/mywatch-plugin-youtube/src/index.tsx
+++ plugins/mywatch-plugin-youtube/src/index.tsx
@@ -1,9 +1,32 @@
 import type { MyWatchPlugin } from '@mywatch/plugin-sdk'
+import { YouTubeCard } from './YouTubeCard'
+import { AddYouTubeItemModal } from './AddYouTubeItemModal'
+import { matchesUrl, fetchYouTubeMetadata } from './utils'
 
-const plugin: MyWatchPlugin = {
+const youtubePlugin: MyWatchPlugin = {
   id: 'youtube',
   displayName: 'YouTube Links',
-  listTypes: [],
+  listTypes: [
+    {
+      id: 'youtube',
+      label: 'YouTube Links',
+      CardComponent: YouTubeCard,
+      AddItemModal: AddYouTubeItemModal,
+      matchesUrl,
+      prefillFromUrl: async (url) => {
+        const meta = await fetchYouTubeMetadata(url)
+        if (!meta) return {}
+        return {
+          url,
+          videoId: meta.videoId,
+          title: meta.title,
+          thumbnail: meta.thumbnail,
+          channelName: meta.channelName,
+          watched: false,
+        }
+      },
+    },
+  ],
 }
 
-export default plugin
+export default youtubePlugin
